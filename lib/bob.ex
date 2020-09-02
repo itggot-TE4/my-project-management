eb75160d8d6e00039764ac3b42ed2fb53acff42b
Linus Styrén <linus.styren@Linuss-MacBook-Pro.local>
2020-09-02 08:57:57 +0200

    refactor using helper methods

--- lib/bob.ex
+++ lib/bob.ex
@@ -1,11 +1,15 @@
 defmodule Bob do
   def hey(input) do
     cond do
-      String.trim(input) == "" -> "Fine. Be that way!"
-      !String.match?(input, ~r/^(\d|\W)+$/) and String.ends_with?(input, "?") and String.upcase(input) == input -> "Calm down, I know what I'm doing!"
-      String.ends_with?(String.trim(input), "?") -> "Sure."
-      !String.match?(input, ~r/^(\d|\W)+$/) and String.upcase(input) == input -> "Whoa, chill out!"
+      silence?(input) -> "Fine. Be that way!"
+      question?(input) and shouting?(input) -> "Calm down, I know what I'm doing!"
+      question?(input) -> "Sure."
+      shouting?(input) -> "Whoa, chill out!"
       true -> "Whatever."
     end
   end
+
+  def silence?(input), do: String.trim(input) == ""
+  def shouting?(input), do: !String.match?(input, ~r/^(\d|\W)+$/) and String.upcase(input) == input
+  def question?(input), do: String.ends_with?(String.trim(input), "?")
 end
